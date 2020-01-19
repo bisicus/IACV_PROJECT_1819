5396cf3360c6b9e97985132abd8597304d9d21cf
Leonardo Bisica <leonardo.bisica@gmail.com>
2020-01-19 21:27:36 +0100

    Bugfixes and renaming
    Correction on crossratio order computation

--- Functions/Finger_Tips/Frame2World_Convertion/FRONT_Tips2World.m
+++ Functions/Finger_Tips/Frame2World_Convertion/FRONT_Tips2World.m
@@ -1,26 +1,55 @@
-function [X_Coord, Z_Coord] = ...
-            FRONT_Tips2World(Front_finger_tips,front_geometric_features)
-%FRONT_TIPS2WORLD Computation of Real World Coordinates of Finger Tips
-% 
-%    PROCEDURE for each Finger Tip:
-%       1) CrossRatio Elements Computation
-%          - Computation of 'line to the horizon' passing through Tip and 
-%            Vanishing Point
-%          - B Point, intersection of 'horizon line' and 'Black Keys
-%            Termination' line
-%          - W Point, intersection of 'horizon line' and 'White Keys
-%            Termination' line
-%       2) Computing CrossRatio for T, B, W, Vp
-%       3) Using Cr quantity to Compute World Coordinates
-%          - TODO:
+function Z_Coord = FRONT_Tips2World( Front_tips, ...
+                                     front_geometric_features, ...
+                                     WORLD_Measures )
+%FRONT_TIPS2WORLD Computes Real World Coordinates of each Finger Tip
+% OUTPUTS:
+%    * Z_Coord - vector of coordinates of finger tips in real world system
+%      coordinates
+%
+%
+% INPUTS:
+%    * Front_tips - matrix of stacked row vector in the form [x, y]
+%
+%    * front_geometric_features - struct containing geometric features such
+%      as point, lines computed from background image
+%
+%    * WORLD_Measures - struct of hardcoded measurements of World Distances
+%
+%
+% PROCEDURE for each Finger Tip:
+%    1) CrossRatio Elements Computation
+%       - Computation of 'line to the horizon' passing through Tip and 
+%         Vanishing Point
+%       - B Point, intersection of 'horizon line' and 'Black Keys
+%         Termination' line
+%       - W Point, intersection of 'horizon line' and 'White Keys
+%         Termination' line
+%
+%    2) Computing CrossRatio for T, B, W, Vp
+%
+%    3) Using Cr quantity to Compute World Coordinates
+%       - Cross-ratio is an invariant on projective transformations such as
+%         the convertion from Image to Real-World system.
+%       - Knowing some immutable measurements of the Real World,
+%         Cross-ratio value can be used to compute depth of a known point
+%         in image ([x,y] coordinates) that is unknown in world.
+%         Refer to tip_2_world_depth documentation for further details
+%
+%
+% NOTE:
+%    - in real world coordinate system origin has been placed where keys
+%      are attached to the plastic control board.
+%      This makes computed Distances valuable as Coordinates
+%
+% See also tip_2_world_depth, cross_ratio, UP_Tips2World
 
-X_Coord = Front_finger_tips(:, 1);
+num_elem = size(Front_tips, 1);
 
-Z_Coord = zeros(size(Front_finger_tips, 1), 1);
+Z_Coord = zeros(num_elem, 1);
 
 
-for kk = 1:size(Front_finger_tips, 1)
-      T = [ Front_finger_tips(kk, :), 1 ];
+for kk = 1:num_elem
+      T = [ Front_tips(kk, :), 1 ]; % Append 1 to make it Homogeneous
       
       horizon_line = homog_cross(T, front_geometric_features.vanish_point);
       B = homog_cross(horizon_line, front_geometric_features.horiz_BlackKey_line);
@@ -34,7 +63,7 @@ for kk = 1:size(Front_finger_tips, 1)
          T, B, W, 
       %}
       
-      C_r = cross_ratio(T, B, W, front_geometric_features.vanish_point);
+      C_r = cross_ratio(B, T, W, front_geometric_features.vanish_point);
       
       Z_Coord(kk) = tip_2_world_depth(WORLD_Measures, C_r);
 end
