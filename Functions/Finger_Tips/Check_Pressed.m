a7d69bc5e54913761b06ea5bcbd9b08cf48cc6e8
Leonardo Bisica <leonardo.bisica@gmail.com>
2020-01-23 14:11:12 +0100

    check_pressed improved
    fingers that respects 'Real World Geometry' that are also really over a black key

--- Functions/Finger_Tips/Check_Pressed.m
+++ Functions/Finger_Tips/Check_Pressed.m
@@ -2,41 +2,36 @@ function [WHT_pressed_idxs, BLK_pressed_idxs] = ...
                   Check_Pressed( Z_Front, Z_Up, ...
                                  angle, BLK_height, ...
                                  Up_Tips, ...
-                                 up_video_struct)
+                                 up_video_struct, ...
+                                 camera_struct)
 %CHECK_PRESSED Summary of this function goes here
 %   Detailed explanation goes here
 
    % ===== Check White Pressed ===== %
    WHT_pressed_idxs = find( Z_Up > Z_Front);
    
-   % Remove 'Pressed White Keys' from input Coordinates, if any
-   Z_Front(WHT_pressed_idxs) = [];
-   Z_Up(WHT_pressed_idxs) = [];
    
    
-   % ===== Check Balck Pressed ===== %
+   % ===== Check Black Pressed ===== %
+   
+   % 1. Real World Finger Height
    base = abs( Z_Front - Z_Up );
    height = base / tan(angle);
    
-   BLK_pressed_idxs = find( height < BLK_height);
    
-   [nearest_key, dists] = find_nearest_idxs( up_video_struct.BLACK_Keys_centerCoord, ...
-                                             Up_Tips, ...
-                                             1, ...
-                                             'method', 'only_x' );
+   % 2. Fingers that are really over Black Keys
+   [~ , dists] = find_nearest_idxs( up_video_struct.BLACK_Keys_centerCoord, ...
+                                    Up_Tips, ...
+                                    1, ...
+                                    'method', 'only_x' );
    
-   
-   
-   % ----- Black Pressed Sanitation ----- % TODO: Aggiungi documentazione
-   % Devo controllare se i tasti identificati sono realmente sopra un tasto
-   % nero. Per farlo il tip deve essere distante dal centroide meno di 2/3
-   % della lunghezza del tasto.
+   BLK_pressed_idxs = find( dists < camera_struct.BLACK_Key_WIDTH *(2/3) & ...
+                            height < BLK_height );
    
    
-   pressed_idxs = sort(pressed_idxs, 'ascend');
-   if ~isempty(pressed_idxs)
-      disp(['The result is: [' num2str(pressed_idxs(:).') '], at frame: ' num2str(frame_counter)]) ;
-   end
-
+   % 3. Remove 'Pressed White Keys' from input Coordinates, if any
+   BLK_pressed_idxs = setdiff(BLK_pressed_idxs, WHT_pressed_idxs);
+  
+   
 end
 
